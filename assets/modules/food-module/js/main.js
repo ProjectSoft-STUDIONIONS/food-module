7d7de2cb56ea03b7562eba5d8d4a8765927f9294
ProjextSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2025-12-12 17:38:34 +0400

    update

--- assets/modules/food-module/js/main.js
+++ assets/modules/food-module/js/main.js
@@ -4,7 +4,7 @@
  * Актуально для сайтов школ России
  * Автор: Чернышёв Андрей aka ProjectSoft <projectsoft2009@yandex.ru>
  * GitHub: https://github.com/ProjectSoft-STUDIONIONS/food-module#readme
- * Last Update: 2025-12-12 17:30:03 GMT+0400
+ * Last Update: 2025-12-12 17:36:48 GMT+0400
  */
 window.DT_table = false;
 (function (factory) {
